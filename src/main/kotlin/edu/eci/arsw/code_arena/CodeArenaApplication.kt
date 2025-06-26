87a42ef3ee9938f84036c2b9d20e1a34d97c86be
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-06-25 23:01:46 -0500

    feat: add code-arena-realtime project structure with Gradle and Kotlin as primary language

---
+++ src/main/kotlin/edu/eci/arsw/code_arena/CodeArenaApplication.kt
@@ -0,0 +1,11 @@
+package edu.eci.arsw.code_arena
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeArenaApplication
+
+fun main(args: Array<String>) {
+	runApplication<CodeArenaApplication>(*args)
+}
---
+++ src/test/kotlin/edu/eci/arsw/code_arena/CodeArenaApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.eci.arsw.code_arena
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodeArenaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
